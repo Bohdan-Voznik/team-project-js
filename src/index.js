6f930bb947cf7463b1d3b51640fba8b1177a6485
Alona Puhach <alen1414@gmail.com>
2022-02-02 15:39:38 -0400

    checkbox/dark

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import './sass/main.scss';
+import Darkmode from 'darkmode-js';
+new Darkmode().showWidget();
+const options = {
+  bottom: '64px', // default: '32px'
+  right: 'unset', // default: '32px'
+  left: '32px', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: '🌖', // default: ''
+  autoMatchOsTheme: true // default: true
+  
+}
+const switchCheckbox = document.querySelector('.switch__checkbox');
+const switchToggle = document.querySelector('.darkmode-toggle');
+console.log(switchToggle);
+
+switchToggle.addEventListener('click', onChangeBg);
+function onChangeBg() {
+  // switchCheckbox.classList.toggle("slider")
+  
+  if (switchCheckbox.checked) switchCheckbox.checked = false
+
+    
+ else switchCheckbox.checked = true;
+ };
