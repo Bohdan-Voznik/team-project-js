d246bf0072eef1b0bf57eff7cbe74a8e337b9b0b
EugeniaAlex <eugenia.aleksandrova@gmail.com>
2022-02-10 14:28:33 +0200

    edit-modals

--- src/js/team-modal-open.js
+++ src/js/team-modal-open.js
@@ -2,6 +2,25 @@ const teamModalOpenLink = document.querySelector('[data-team-modal-open]');
 const teamModalCloseBtn = document.querySelector('[data-team-modal-close]');
 const teamModalWindow = document.querySelector('[data-team-modal]');
 const backdropTeamModal = document.querySelector('.team-modal-backdrop');
+function disabledBodyScroll() {
+  let pagePosition = window.scrollY;
+  document.body.classList.add('disable-scroll');
+  document.body.dataset.position = pagePosition;
+  console.log(pagePosition);
+  window.scrollTo({ top: pagePosition });
+  document.body.style.top = `${-pagePosition}px`;
+  document.body.style.position = 'fixed';
+}
+
+function enabledBodyScroll() {
+  let pagePosition = parseInt(document.body.dataset.position, 10);
+  document.body.style.position = 'static';
+  document.body.style.top = 'auto';
+  document.body.classList.remove('disable-scroll');
+  window.scroll({ top: pagePosition, left: 0 });
+  document.body.removeAttribute('data-position');
+}
+
 
 backdropTeamModal.addEventListener('click', e => {
   if (e.target === e.currentTarget) {
@@ -24,15 +43,12 @@ function teamModalClickToggle(event) {
   document.addEventListener('keydown', onEscapeClickTeamModal);
   event.preventDefault();
   teamModalWindow.classList.toggle('is-hidden');
-  // if (!teamModalWindow.classList.contains('is-hidden')) {
-  //   document.body.classList.toggle('.no-scroll');
-  //   document.body.style.overflow = 'hidden';
-  //   console.log(document.body.classList);
-  //   console.log(teamModalWindow.classList);
-  // } else {
-  //   document.body.style.overflow = 'auto';
-  //   document.body.classList.toggle('.no-scroll');
-  //   console.log(document.body.classList);
-  //   console.log(teamModalWindow.classList);
-  // }
+
+  if (teamModalWindow.classList.contains('is-hidden')) {
+    enabledBodyScroll();
+  }
+  else {
+    disabledBodyScroll();
+  }
+  
 }
