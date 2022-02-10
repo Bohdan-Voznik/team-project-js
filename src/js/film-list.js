9dfb23407ec47385956adb72fb5c28a1302f42a3
Pavel <kossoua88@gmail.com>
2022-02-10 19:12:34 +0200

    bug fixed

--- src/js/film-list.js
+++ src/js/film-list.js
@@ -2,11 +2,16 @@ import filmInfo from '../partials/film-item.hbs';
 
 export function createMarkup(films, lang) {
   const filmsList = films.map(film => {
+    const genreEn = film.genreEn;
+    const genreUk = film.genreUk;
+    console.log(genreEn);
+    console.log(genreUk);
+
     return filmInfo({
       image: lang === 'en' ? film.imageEn : film.imageUk,
       title: lang === 'en' ? film.titleEn : film.titleUk,
       genre:
-        lang === 'en' ? film.genreEn.slice(0, 2).join(', ') : film.genreUk.slice(0, 2).join(', '),
+        lang === 'en' ? genreEn.slice(0, 2).join(', ') : genreUk.slice(0, 2).join(', '),
       reliseData: film.reliseData,
       vote: film.vote,
       id: film.id,
